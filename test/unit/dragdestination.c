9054c28df87e983b40f12eb43a21ef85c600fa11
Sanko Robinson <30663+sanko@users.noreply.github.com>
2026-08-12 20:53:02 -0400

    Fix dragdestination unit tests for GCC/Clang -Wincompatible-pointer-types
    Use the typed uiLabel pointer from the test state (matching tooltip.c)
    and wrap with uiControl() when registering drag destinations.

--- test/unit/dragdestination.c
+++ test/unit/dragdestination.c
@@ -1,63 +1,63 @@
 #include "unit.h"
 
-#define uiControlPtrFromUnitState(s) uiControlPtrFromState(uiControl, s)
+#define uiLabelPtrFromState(s) uiControlPtrFromState(uiLabel, s)
 
 static void dragDestinationNew(void **state)
 {
-	uiControl **c = uiControlPtrFromUnitState(state);
+	uiLabel **c = uiLabelPtrFromState(state);
 	uiDragDestination *dd;
 
 	*c = uiNewLabel("drag destination");
 	dd = uiNewDragDestination();
 	assert_non_null(dd);
-	uiControlRegisterDragDestination(*c, dd);
+	uiControlRegisterDragDestination(uiControl(*c), dd);
 }
 
 static void dragDestinationAcceptTypesDefault(void **state)
 {
-	uiControl **c = uiControlPtrFromUnitState(state);
+	uiLabel **c = uiLabelPtrFromState(state);
 	uiDragDestination *dd;
 
 	*c = uiNewLabel("drag destination");
 	dd = uiNewDragDestination();
-	uiControlRegisterDragDestination(*c, dd);
+	uiControlRegisterDragDestination(uiControl(*c), dd);
 	assert_int_equal(uiDragDestinationAcceptTypes(dd), 0);
 }
 
 static void dragDestinationSetAcceptTypes(void **state)
 {
-	uiControl **c = uiControlPtrFromUnitState(state);
+	uiLabel **c = uiLabelPtrFromState(state);
 	uiDragDestination *dd;
 
 	*c = uiNewLabel("drag destination");
 	dd = uiNewDragDestination();
 	uiDragDestinationSetAcceptTypes(dd, uiDragTypeText | uiDragTypeURIs);
-	uiControlRegisterDragDestination(*c, dd);
+	uiControlRegisterDragDestination(uiControl(*c), dd);
 	assert_int_equal(uiDragDestinationAcceptTypes(dd), uiDragTypeText | uiDragTypeURIs);
 }
 
 static void dragDestinationLastOperationDefault(void **state)
 {
-	uiControl **c = uiControlPtrFromUnitState(state);
+	uiLabel **c = uiLabelPtrFromState(state);
 	uiDragDestination *dd;
 
 	*c = uiNewLabel("drag destination");
 	dd = uiNewDragDestination();
-	uiControlRegisterDragDestination(*c, dd);
+	uiControlRegisterDragDestination(uiControl(*c), dd);
 	assert_int_equal(uiDragDestinationLastDragOperation(dd), uiDragOperationNone);
 }
 
 static void dragDestinationRegisterTwice(void **state)
 {
-	uiControl **c = uiControlPtrFromUnitState(state);
+	uiLabel **c = uiLabelPtrFromState(state);
 	uiDragDestination *dd1;
 	uiDragDestination *dd2;
 
 	*c = uiNewLabel("drag destination");
 	dd1 = uiNewDragDestination();
 	dd2 = uiNewDragDestination();
-	uiControlRegisterDragDestination(*c, dd1);
-	uiControlRegisterDragDestination(*c, dd2);
+	uiControlRegisterDragDestination(uiControl(*c), dd1);
+	uiControlRegisterDragDestination(uiControl(*c), dd2);
 }
 
 #define dragDestinationUnitTest(f) cmocka_unit_test_setup_teardown((f), \
